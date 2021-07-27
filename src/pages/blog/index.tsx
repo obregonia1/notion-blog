bceb9ef27878092af7d31c79cbb3803027cc480c
obregonia1 <n.y.skate.of.minds@gmail.com>
2021-07-28 08:58:03 +0900

    公式APIの投稿リストを使用

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,50 +4,16 @@ import Header from '../../components/header'
 import blogStyles from '../../styles/blog.module.css'
 import sharedStyles from '../../styles/shared.module.css'
 
-import {
-  getBlogLink,
-  getTagLink,
-  getDateStr,
-  postIsPublished,
-} from '../../lib/blog-helpers'
+import { getBlogLink, getTagLink } from '../../lib/blog-helpers'
 import { textBlock } from '../../lib/notion/renderers'
-import getNotionUsers from '../../lib/notion/getNotionUsers'
-import getBlogIndex from '../../lib/notion/getBlogIndex'
+import { getPosts, getAllTags } from '../../lib/notion/client'
 
-export async function getStaticProps({ preview }) {
-  const postsTable = await getBlogIndex()
-
-  const authorsToGet: Set<string> = new Set()
-  const posts: any[] = Object.keys(postsTable)
-    .map((slug) => {
-      const post = postsTable[slug]
-      // remove draft posts in production
-      if (!preview && !postIsPublished(post)) {
-        return null
-      }
-      post.Authors = post.Authors || []
-      for (const author of post.Authors) {
-        authorsToGet.add(author)
-      }
-      return post
-    })
-    .filter(Boolean)
-
-  const tags: string[] = Object.keys(postsTable)
-    .filter((slug) => postIsPublished(postsTable[slug]))
-    .map((slug) => postsTable[slug].Tags)
-    .flat()
-    .filter((tag, index, self) => self.indexOf(tag) === index)
-
-  const { users } = await getNotionUsers([...authorsToGet])
-
-  posts.map((post) => {
-    post.Authors = post.Authors.map((id) => users[id].full_name)
-  })
+export async function getStaticProps() {
+  const posts = await getPosts()
+  const tags = await getAllTags()
 
   return {
     props: {
-      preview: preview || false,
       posts,
       tags,
     },
@@ -55,21 +21,10 @@ export async function getStaticProps({ preview }) {
   }
 }
 
-const Index = ({ posts = [], tags = [], preview }) => {
+const Index = ({ posts = [], tags = [] }) => {
   return (
     <>
       <Header titlePre="Blog" />
-      {preview && (
-        <div className={blogStyles.previewAlertContainer}>
-          <div className={blogStyles.previewAlert}>
-            <b>Note:</b>
-            {` `}Viewing in preview mode{' '}
-            <Link href={`/api/clear-preview`}>
-              <button className={blogStyles.escapePreview}>Exit Preview</button>
-            </Link>
-          </div>
-        </div>
-      )}
       <div className={`${sharedStyles.layout} ${blogStyles.blogIndex}`}>
         <h1>Code Rules Everything Around Me</h1>
         {posts.length === 0 && (
@@ -78,22 +33,16 @@ const Index = ({ posts = [], tags = [], preview }) => {
         {posts.map((post) => {
           return (
             <div className={blogStyles.postPreview} key={post.Slug}>
+              {post.Date && (
+                <div className="posted">📅&nbsp;&nbsp;{post.Date}</div>
+              )}
               <h3>
                 <span className={blogStyles.titleContainer}>
-                  {!post.Published && (
-                    <span className={blogStyles.draftBadge}>Draft</span>
-                  )}
                   <Link href="/blog/[slug]" as={getBlogLink(post.Slug)}>
-                    <a>{post.Page}</a>
+                    <a>{post.Title}</a>
                   </Link>
                 </span>
               </h3>
-              {/*{post.Authors.length > 0 && (*/}
-              {/*  <div className="authors">By: {post.Authors.join(' ')}</div>*/}
-              {/*)}*/}
-              {post.Date && (
-                <div className="posted">Posted: {getDateStr(post.Date)}</div>
-              )}
               {post.Tags &&
                 post.Tags.length > 0 &&
                 post.Tags.map((tag) => (
@@ -106,13 +55,10 @@ const Index = ({ posts = [], tags = [], preview }) => {
                     <a className={blogStyles.tag}>🔖{tag}</a>
                   </Link>
                 ))}
-              <p>
-                {(!post.preview || post.preview.length === 0) &&
-                  'No preview available'}
-                {(post.preview || []).map((block, idx) =>
-                  textBlock(block, true, `${post.Slug}${idx}`)
-                )}
-              </p>
+              <p>{post.Excerpt}</p>
+              <Link href="/blog/[slug]" as={getBlogLink(post.Slug)} passHref>
+                <a className={blogStyles.expandButton}>続きを読む</a>
+              </Link>
             </div>
           )
         })}
